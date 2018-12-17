9ba527e078e22f9299683e2caec895c94a8ce2af
manny agbayani <manny.i.agbayani@gmail.com>
2018-12-17 18:49:05 +1300

    added State Variables, Constructor, Fallback , EnterLottery and GetBalance

--- Contracts/Lottery.sol
+++ Contracts/Lottery.sol
@@ -1,5 +1,37 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0 <= 0.6.0;
 
 contract Lottery {
-    address manager;
+    
+    //----STATE VARIABLES-----
+    //Manager = address of the EOA that deployed the contract and start the Lottery
+    address public Manager;
+    
+    //List of all players
+    address[] public PlayerAddresses;
+    
+    
+    //----CONSTRUCTOR-----
+    constructor() public {
+        //initialized the Manager state variable
+        Manager = msg.sender;
+    }
+    
+    //----FUNCTIONS-----
+    //Callback function that will add address to PlayerAddresses
+    function () external payable {
+        PlayerAddresses.push(msg.sender); 
+    }
+    
+    //Calling this function will add the address to the PlayerAddresses
+    function EnterLottery() payable public {
+        PlayerAddresses.push(msg.sender);
+    }
+    
+    //Calling this function will return the current balance of the contract
+    function GetBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
+    
+    
 }
